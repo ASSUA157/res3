844fa28b2f882aa34236acf29742abf7c3dcdb4c
FYW <1575440020@qq.com>
2020-04-12 19:47:20 +0800

    添加add方法

--- git-test/src/test.java
+++ git-test/src/test.java
@@ -1,9 +1,12 @@
 public class test {
     public static void main(String[] args) {
-        System.out.printf("hello git!!!");
-        System.out.printf("hello git!!!");
-        System.out.printf("hello git!!!");
-        System.out.printf("hello git!!!");
-
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+    }
+    public void add(){
+        int a = 1;
+        int b = 2 ;
+        System.out.println(a + b);
     }
 }
